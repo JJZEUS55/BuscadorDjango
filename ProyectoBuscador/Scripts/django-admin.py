111b200fd1e40a2901193212f866539742b9e5a6
JJZEUS55 <geoge55@hotmail.com>
2019-02-14 07:26:56 -0600

    config django

---
+++ ProyectoBuscador/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jjzeus55\desktop\trabajo\buscadordjango\proyectobuscador\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
